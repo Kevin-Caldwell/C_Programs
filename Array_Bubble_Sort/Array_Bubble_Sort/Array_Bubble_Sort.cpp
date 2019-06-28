69d614568c0ab93169fd8a60c5c37e2dea1790e7
Kevin Caldwell <kevinc2004@gmail.com>
2019-06-27 23:31:56 -0400

    My first program on bubble sort using c

---
+++ Array_Bubble_Sort/Array_Bubble_Sort/Array_Bubble_Sort.cpp
@@ -0,0 +1,66 @@
+#include <conio.h>
+#include <iostream>
+
+int arr_length;
+
+void swap(int &num1, int &num2){
+
+	int temp = num1;
+
+	num1 = num2;
+	num2 = temp;
+
+}
+
+int* bubble_sort(int arr[]){
+
+	for (int j = 0; j < arr_length; j++)
+	{
+
+
+		for (int i = 1; i < arr_length; i++)
+		{
+			if(arr[i] < arr[i - 1]){
+
+				swap(arr[i], arr[i - 1]);
+			}
+		}
+	}
+
+	return arr;
+}
+
+void print_arr(int arr[])
+{
+	using namespace std;
+
+	cout << "Array : ";
+
+	for (int i = 0; i < arr_length; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n";
+}
+
+void main(){
+	std::cout << "Enter the number of elements: ";
+	std::cin >> arr_length;
+	std::cout << "\n";
+
+	int *arr = new int[arr_length];
+
+	for (int i = 0; i < arr_length; i++)
+	{
+		std::cout << "Enter Array Element number " << (i + 1) << " : ";
+		std::cin >> arr[i];
+		std::cout << "\n";
+	}
+
+	arr = bubble_sort(arr);
+
+	print_arr(arr);
+
+	_getch();
+}
